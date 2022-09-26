4b01974041817ee5e4d51ab654aa2846446acc64
Chris Hayes <6013871+Christopher-Hayes@users.noreply.github.com>
2022-09-26 13:25:51 -0400

    :sparkles: Add Wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+return {
+  scrollback_lines = 5000,
+  color_scheme = "Gruvbox Dark",
+  font = wezterm.font 'JetBrains Mono',
+  window_close_confirmation = "NeverPrompt",
+}
+
